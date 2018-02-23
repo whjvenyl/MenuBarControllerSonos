8b844844127259d6d3d8a436279cd013c7859331
Alexander Heinrich <alexander@sn0wfreeze.de>
2018-02-23 22:33:11 +0100

    Initial Commit

---
+++ Sonos_Volume_Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sonos_Volume_Control
+//
+//  Created by Alexander Heinrich on 23.02.18.
+//  Copyright © 2018 Sn0wfreeze Development UG. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
